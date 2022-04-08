331dbdab69384342062250f771273b499c63d262
TonyBrobston <anthonybrobston@live.com>
2022-04-08 12:45:04 -0500

    Pass minDegreesTraveled rather than using a global

--- vent_local_mqtt/vent_local_mqtt.ino
+++ vent_local_mqtt/vent_local_mqtt.ino
@@ -8,9 +8,6 @@ int servoSensorPin = A0;
 int upperLimit = 100;
 int threshold = 165;
 
-int globalStartPosition = 90;
-int minDegreesTraveled = 40;
-
 int openedPosition;
 int closedPosition;
 
@@ -21,10 +18,10 @@ void setup() {
   pinMode(INPUT, servoSensorPin);
   pinMode(OUTPUT, servoSensorPin);
 
-  closedPosition = close(globalStartPosition);
+  closedPosition = close(90, 40);
   Serial.print("closedPosition: ");
   Serial.println(closedPosition);
-  openedPosition = open(closedPosition);
+  openedPosition = open(closedPosition, 90);
   Serial.print("openedPosition: ");
   Serial.println(openedPosition);
 }
@@ -32,13 +29,13 @@ void setup() {
 void loop() {
 }
 
-int open(int startPosition) {
+int open(int startPosition, int minDegreesTraveled) {
   servo.attach(servoOutputPin, servoMin, servoMax);
   int maxPosition = 180;
 
   for(int position = startPosition; position < maxPosition; position++) {
     int degreesTraveled = position - startPosition;
-    if (turnOneDegreeUnlessAtEndStop(position, degreesTraveled)) {
+    if (turnOneDegreeUnlessAtEndStop(position, degreesTraveled, minDegreesTraveled)) {
       return position;
     }  
   }
@@ -46,13 +43,13 @@ int open(int startPosition) {
   return maxPosition;
 }
 
-int close(int startPosition) {
+int close(int startPosition, int minDegreesTraveled) {
   servo.attach(servoOutputPin, servoMin, servoMax);
   int minPosition = 0;
 
   for(int position = startPosition; position > minPosition; position--) {
     int degreesTraveled = startPosition - position;
-    if (turnOneDegreeUnlessAtEndStop(position, degreesTraveled)) {
+    if (turnOneDegreeUnlessAtEndStop(position, degreesTraveled, minDegreesTraveled)) {
       return position;
     }  
   }
@@ -60,7 +57,7 @@ int close(int startPosition) {
   return minPosition;
 }
 
-bool turnOneDegreeUnlessAtEndStop(int position, int degreesTraveled) {
+bool turnOneDegreeUnlessAtEndStop(int position, int degreesTraveled, int minDegreesTraveled) {
   bool hasTraveledFarEnough = degreesTraveled >= minDegreesTraveled;
   int servoSensorValue = analogRead(servoSensorPin);
   bool hasHitVoltageThreshold = servoSensorValue >= threshold;
