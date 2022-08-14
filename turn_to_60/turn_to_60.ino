53e78de25dee85e296b348caab713bc4bdf51873
Tony Brobston <anthonybrobston@live.com>
2022-08-14 15:12:34 -0500

    Update turn_to_60.ino

--- turn_to_60/turn_to_60.ino
+++ turn_to_60/turn_to_60.ino
@@ -2,14 +2,11 @@
 
 Servo servo;
 const int servoPin = 0;
-const int servoPotentiometerPin = A0;
 const int servoMin = 500;
 const int servoMax = 3750;
 
 void setup() {
-  Serial.begin(9600);
   pinMode(servoPin, OUTPUT);
-  pinMode(servoPotentiometerPin, INPUT);
   servo.attach(servoPin, servoMin, servoMax);
 }
 
